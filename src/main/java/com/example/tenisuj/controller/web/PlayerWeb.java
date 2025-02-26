50ff16387628f5815391cd7cc7f8ffd0c92011eb
marek krncok <krncok36@gmail.com>
2025-02-26 21:30:17 +0100

    fix profile/create profile

--- src/main/java/com/example/tenisuj/controller/web/PlayerWeb.java
+++ src/main/java/com/example/tenisuj/controller/web/PlayerWeb.java
@@ -3,6 +3,7 @@ package com.example.tenisuj.controller.web;
 import com.example.tenisuj.model.Player;
 import com.example.tenisuj.model.dto.PlayerDTO;
 import com.example.tenisuj.service.PlayerService;
+import com.example.tenisuj.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
@@ -20,48 +21,55 @@ import java.util.stream.Collectors;
 public class PlayerWeb {
 
     private final PlayerService playerService;
+    private final UserService userService;
 
     @Autowired
-    public PlayerWeb(PlayerService playerService) {
+    public PlayerWeb(PlayerService playerService, UserService userService) {
         this.playerService = playerService;
+        this.userService = userService;
     }
 
     @GetMapping("/")
-    String getAllPlayers(Model model, @Param("keyword") String keyword) {
-        setDefaultValues(model);
+    String getAllPlayers(Model model, @Param("keyword") String keyword,Principal principal) {
+        setDefaultValues(model,principal);
         List<Player> players = playerService.getAllPlayers(keyword);
         List<PlayerDTO> playerDTOs = players.stream()
                 .map(PlayerDTO::new)
                 .collect(Collectors.toList());
         model.addAttribute("players", playerDTOs);
-        model.addAttribute("player", new Player());
+        model.addAttribute("newPlayer", new Player());
         model.addAttribute("keyword", keyword);
         return "players";
     }
 
     @PostMapping("/create")
-    String createPlayer(Model model, @ModelAttribute("player") Player player) {
-        setDefaultValues(model);
+    String createPlayer(@ModelAttribute("player") Player player) {
         playerService.addPlayer(player.getFirstName(), player.getLastName(), player.getEmail(), player.getGender(), player.getBirthDate(), player.getLeagueStatus(), player.getHand(), player.getRating(), player.getRegistrationDate());
         return "redirect:/players/";
     }
 
     @GetMapping("/{id}")
-    public String showEditPlayerForm(@PathVariable("id") String playerId, Model model) {
-        setDefaultValues(model);
+    public String showEditPlayerForm(@PathVariable("id") String playerId, Model model,Principal principal) {
+        setDefaultValues(model,principal);
         model.addAttribute("player", playerService.getPlayerById(playerId));
         return "playerEdit";
     }
 
     @PostMapping("/{id}")
-    public String editPlayer(@PathVariable("id") String playerId, Model model, @ModelAttribute("player") Player player) {
-        setDefaultValues(model);
-        playerService.editPlayer(playerId, player.getFirstName(),player.getLastName(),player.getEmail(),player.getGender(),player.getBirthDate(),player.getLeagueStatus(),player.getHand(),player.getRating());
+    public String editPlayer(@PathVariable("id") String playerId, @ModelAttribute("player") Player player) {
+        playerService.editPlayer(playerId, player.getFirstName(), player.getLastName(), player.getEmail(), player.getGender(), player.getBirthDate(), player.getLeagueStatus(), player.getHand(), player.getRating());
         log.info("player edited {}", playerId);
         return "redirect:/players/";
     }
 
-    private void setDefaultValues(Model model) {
+    private void setDefaultValues(Model model,Principal principal) {
         model.addAttribute("pageTitle", "Players");
+        if (principal != null) {
+            if (userService.getUser(principal.getName()).getPlayer() != null) {
+                model.addAttribute("user", userService.getUser(principal.getName()));
+                model.addAttribute("player", playerService.getPlayerById(userService.getUser(principal.getName()).getPlayer().getId()));
+            } else {
+                model.addAttribute("player", null);}
+        }
     }
 }
