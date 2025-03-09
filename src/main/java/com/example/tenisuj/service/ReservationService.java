33b7d460a6274438b57c28071b5e46493bb1ce4e
marek krncok <krncok36@gmail.com>
2025-03-09 20:51:40 +0100

    reservationApi

--- src/main/java/com/example/tenisuj/service/ReservationService.java
+++ src/main/java/com/example/tenisuj/service/ReservationService.java
@@ -1,10 +1,14 @@
 package com.example.tenisuj.service;
 
+import com.example.tenisuj.model.Reservation;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 
 public interface ReservationService {
 
     boolean isAvailable(String place, LocalDate date, LocalTime startTime, LocalTime endTime);
-    void createReservation(String place,LocalDate date, LocalTime startTime,LocalTime endTime);
+    void createReservation(String place, LocalDate date, LocalTime startTime, LocalTime endTime);
+    List<Reservation> getAllReservations();
+    void deleteReservation(String id);
 }
