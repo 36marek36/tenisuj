e7a35e8a1f03c41e119949c0263deb42b210e0a9
marek krncok <krncok36@gmail.com>
2025-03-16 18:23:01 +0100

    Reservation Graph to Reservation Template

--- src/main/java/com/example/tenisuj/service/ReservationService.java
+++ src/main/java/com/example/tenisuj/service/ReservationService.java
@@ -2,6 +2,8 @@ package com.example.tenisuj.service;
 
 import com.example.tenisuj.model.Match;
 import com.example.tenisuj.model.Reservation;
+import com.example.tenisuj.model.ReservationTimeSlot;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
@@ -14,4 +16,6 @@ public interface ReservationService {
     void deleteReservation(String id);
     void approveReservation(String reservationId);
     List<Reservation> getAllPlayerReservation(String playerId);
+    boolean isTimeReserved (LocalTime time,List<Reservation> reservations);
+    List<ReservationTimeSlot> generateTimeSlots (List<Reservation> reservations, LocalDate date);
 }
