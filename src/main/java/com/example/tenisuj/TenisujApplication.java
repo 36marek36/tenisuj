bd30485bb6afa4f805fca0f9550e4f529fec8725
marek krncok <krncok36@gmail.com>
2025-04-06 12:58:07 +0200

    delete CML, my matches from menu to home page,

--- src/main/java/com/example/tenisuj/TenisujApplication.java
+++ src/main/java/com/example/tenisuj/TenisujApplication.java
@@ -23,23 +23,23 @@ import java.util.UUID;
 
 
 @SpringBootApplication
-public class TenisujApplication implements CommandLineRunner {
+public class TenisujApplication{
 
 
-    private final UserRepository userRepository;
-    private final PlayerRepository playerRepository;
-    private final MatchRepository matchRepository;
-    private final LeagueRepository leagueRepository;
-    private final PasswordEncoder passwordEncoder;
-
-    @Autowired
-    public TenisujApplication(UserRepository userRepository, PlayerRepository playerRepository, MatchRepository matchRepository, LeagueRepository leagueRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.playerRepository = playerRepository;
-        this.matchRepository = matchRepository;
-        this.leagueRepository = leagueRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
+//    private final UserRepository userRepository;
+//    private final PlayerRepository playerRepository;
+//    private final MatchRepository matchRepository;
+//    private final LeagueRepository leagueRepository;
+//    private final PasswordEncoder passwordEncoder;
+//
+//    @Autowired
+//    public TenisujApplication(UserRepository userRepository, PlayerRepository playerRepository, MatchRepository matchRepository, LeagueRepository leagueRepository, PasswordEncoder passwordEncoder) {
+//        this.userRepository = userRepository;
+//        this.playerRepository = playerRepository;
+//        this.matchRepository = matchRepository;
+//        this.leagueRepository = leagueRepository;
+//        this.passwordEncoder = passwordEncoder;
+//    }
 
 
     public static void main(String[] args) {
@@ -47,46 +47,46 @@ public class TenisujApplication implements CommandLineRunner {
     }
 
 
-    @Override
-    public void run(String... args) throws Exception {
-        List<User> users = new ArrayList<>(List.of(
-                new User("Marek", "USER", passwordEncoder.encode("123"))
-        ));
+//    @Override
+//    public void run(String... args) throws Exception {
+//        List<User> users = new ArrayList<>(List.of(
+//                new User("Marek", "USER", passwordEncoder.encode("123"))
+//        ));
+//
+//        List<Player> players = new ArrayList<>(List.of(
+//                new Player("00000000-0000-0000-0000-000000000000", "Marek", "priezvisko", "email@email.com", "gender", LocalDate.now(), true,"00000000-0000-0000-0000-000000000100", "L", 0,0, LocalDate.now()),
+//                new Player("00000000-0000-0000-0000-000000000001", "Peter", "priezvisko", "email@email.com", "gender", LocalDate.now(), true,"00000000-0000-0000-0000-000000000100", "L", 0,0, LocalDate.now()),
+//                new Player("00000000-0000-0000-0000-000000000002", "Viktor", "priezvisko", "email@email.com", "gender", LocalDate.now(), true,"00000000-0000-0000-0000-000000000100", "L", 0,0, LocalDate.now()),
+//                new Player("00000000-0000-0000-0000-000000000003", "David", "priezvisko", "email@email.com", "gender", LocalDate.now(), true,"00000000-0000-0000-0000-000000000100", "L", 0,0, LocalDate.now()),
+//                new Player("00000000-0000-0000-0000-000000000004", "Oktan", "priezvisko", "email@email.com", "gender", LocalDate.now(), true,"00000000-0000-0000-0000-000000000100", "L", 0,0, LocalDate.now())
+//        ));
+//
+//        Player player1 = new Player("00000000-0000-0000-0000-000000000005", "Adam", "priezvisko", "email@email.com", "gender", LocalDate.of(2000,1,31), true,null, "L", 0,0, LocalDate.now());
+//        Player player2 = new Player("00000000-0000-0000-0000-000000000006", "Martin", "priezvisko", "email@email.com", "gender", LocalDate.of(2010,1,31), true,null, "L", 0,0, LocalDate.now());
+//
+//        Match match1 = new Match("00000000-0000-0000-0000-000000000010",players.get(0),player1,null, LocalDateTime.of(2025,1,31,10,30),7,5,6,4,null,null,null,null,null,null,null,players.get(0),null,null);
+//        Match match2 = new Match("00000000-0000-0000-0000-000000000011",players.get(1),players.get(2),null,null,7,5,0,6,3,0,null,null,null,null,players.get(1),players.get(2),null,null);
+//
+//        League league = new League("00000000-0000-0000-0000-000000000100","League",players,null,null, LeagueStatus.CREATED);
+//
+//        users.getFirst().setPlayer(players.get(1));
+//        league.setPlayers(players);
+//
+//        playerRepository.saveAll(players);
+//        playerRepository.save(player1);
+//        playerRepository.save(player2);
+//
+//        for (User user : users) {
+//            userRepository.save(user);
+//        }
+//
+//        matchRepository.save(match1);
+//        matchRepository.save(match2);
+//
+//        leagueRepository.save(league);
 
-        List<Player> players = new ArrayList<>(List.of(
-                new Player("00000000-0000-0000-0000-000000000000", "Marek", "priezvisko", "email@email.com", "gender", LocalDate.now(), true,"00000000-0000-0000-0000-000000000100", "L", 0,0, LocalDate.now()),
-                new Player("00000000-0000-0000-0000-000000000001", "Peter", "priezvisko", "email@email.com", "gender", LocalDate.now(), true,"00000000-0000-0000-0000-000000000100", "L", 0,0, LocalDate.now()),
-                new Player("00000000-0000-0000-0000-000000000002", "Viktor", "priezvisko", "email@email.com", "gender", LocalDate.now(), true,"00000000-0000-0000-0000-000000000100", "L", 0,0, LocalDate.now()),
-                new Player("00000000-0000-0000-0000-000000000003", "David", "priezvisko", "email@email.com", "gender", LocalDate.now(), true,"00000000-0000-0000-0000-000000000100", "L", 0,0, LocalDate.now()),
-                new Player("00000000-0000-0000-0000-000000000004", "Oktan", "priezvisko", "email@email.com", "gender", LocalDate.now(), true,"00000000-0000-0000-0000-000000000100", "L", 0,0, LocalDate.now())
-        ));
 
-        Player player1 = new Player("00000000-0000-0000-0000-000000000005", "Adam", "priezvisko", "email@email.com", "gender", LocalDate.of(2000,1,31), true,null, "L", 0,0, LocalDate.now());
-        Player player2 = new Player("00000000-0000-0000-0000-000000000006", "Martin", "priezvisko", "email@email.com", "gender", LocalDate.of(2010,1,31), true,null, "L", 0,0, LocalDate.now());
-
-        Match match1 = new Match("00000000-0000-0000-0000-000000000010",players.get(0),player1,null, LocalDateTime.of(2025,1,31,10,30),7,5,6,4,null,null,null,null,null,null,null,players.get(0),null,null);
-        Match match2 = new Match("00000000-0000-0000-0000-000000000011",players.get(1),players.get(2),null,null,7,5,0,6,3,0,null,null,null,null,players.get(1),players.get(2),null,null);
-
-        League league = new League("00000000-0000-0000-0000-000000000100","League",players,null,null, LeagueStatus.CREATED);
-
-        users.getFirst().setPlayer(players.get(1));
-        league.setPlayers(players);
-
-        playerRepository.saveAll(players);
-        playerRepository.save(player1);
-        playerRepository.save(player2);
-
-        for (User user : users) {
-            userRepository.save(user);
-        }
-
-        matchRepository.save(match1);
-        matchRepository.save(match2);
-
-        leagueRepository.save(league);
-
-
-    }
+//    }
 
 
 }
